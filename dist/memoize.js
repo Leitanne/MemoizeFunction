4bb8e0c30ea4f61e6d55a19d8bdf1d255902c5bc
Leitanne <gina.rico.llorca@gmail.com>
2023-07-04 23:56:33 +0200

    Implementada la función memoize

--- dist/memoize.js
+++ dist/memoize.js
@@ -1,18 +1,16 @@
+"use strict";
 // https://delacruz.dev/blog/memoize-en-javascript
-
-function memoize(callback: (...args: any[]) => any) {
-    let diccionario: Record<string, any> = {};
-
-    return function(...args: any[]){
+function memoize(callback) {
+    let diccionario = {};
+    return function (...args) {
         const clave = args.join("_"); //cada argumento se une al string con _;
-
         // Comprueba el diccionario
-        if(diccionario.hasOwnProperty(clave)) {
+        if (diccionario.hasOwnProperty(clave)) {
             console.log("Valor ya almacenado almacenado");
-        }else{
+        }
+        else {
             diccionario[clave] = callback(...args);
         }
-
         return diccionario[clave];
-    }
+    };
 }
